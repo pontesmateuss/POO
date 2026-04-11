218ed0ef29f0fa30bcc80c204efb7c25eeaf91fa
mateus <mateuspontesdsilva11@gmail.com>
2026-04-11 11:42:27 -0300

    ...

--- python/encapsulamento/EX1.py
+++ python/encapsulamento/EX1.py
@@ -8,7 +8,7 @@ class Estudante:
         self.__creditos = creditos
     
     def detalhar(self):
-        print(f"{self.id}, {self.nome}")
+        print(f"{self._id}, {self.nome}")
 
     def get_creditos(self):
         return self.__creditos
@@ -25,16 +25,14 @@ class Estudante:
         print(f"créditos: {self.__creditos}")
 
 #atributos
-estudante1 = Estudante(1, "Will")
+estudante1 = Estudante(1, "Kobe")
 estudante2 = Estudante(2, "LeBron")
 #metodo
-estudante1.get_creditos(738)
-estudante2.get_creditos(100)
+estudante1.get_creditos(200)
+estudante2.set_creditos(100)
 
-estudante1.set_creditos(99)
-estudante2.set_creditos(89)
+estudante1.get_creditos(100)
+estudante2.set_creditos(50)
 #detalhamento
 estudante1.detalhar()
 estudante2.detalhar()
-
-

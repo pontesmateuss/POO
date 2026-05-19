135fc38bc12a70d9252dcd8a7935004883ca7d14
mateus <mateuspontesdsilva11@gmail.com>
2026-05-18 22:20:24 -0300

    Update EX-1.py

--- exercicios/8-colecoes/EX-1.py
+++ exercicios/8-colecoes/EX-1.py
@@ -6,4 +6,4 @@ print("  Pontec Lotery  ")
 sorteio = random.sample(range(1, 77), 13)
 sorteio.sort()
 
-print(f"números sorteados são: \n{sorteio}")
+print(f"numeros serteados foram estes: \n{sorteio}")
